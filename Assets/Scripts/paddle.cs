f6923b700ecfcaf4f62a7e2cf79d4b9db9e55aca
Jeremy B <workjeremyb@gmail.com>
2024-07-16 19:21:12 -0700

    adding

--- Assets/Scripts/paddle.cs
+++ Assets/Scripts/paddle.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class paddle : MonoBehaviour
 {
+    Vector2 startPos = new Vector2(0f, -4.5f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.position = startPos;
     }
 
     // Update is called once per frame
